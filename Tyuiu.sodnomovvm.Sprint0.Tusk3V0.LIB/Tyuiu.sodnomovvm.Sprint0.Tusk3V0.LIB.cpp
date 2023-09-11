2cbd90ea4993bcab78f51b086e3a8e3d16c9e5ce
tyuiu.sodnomovvm@outlook.com <deek.ren.gg@gmail.com>
2023-09-11 23:15:13 +0500

    Tusk3

---
+++ Tyuiu.sodnomovvm.Sprint0.Tusk3V0.LIB/Tyuiu.sodnomovvm.Sprint0.Tusk3V0.LIB.cpp
@@ -0,0 +1,15 @@
+﻿// Tyuiu.sodnomovvm.Sprint0.Tusk3V0.LIB.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "../../Tyuiu.Cours.cpp/Tyuiu.Cours.cpp.cpp"
+// TODO: Это пример библиотечной функции.
+class Service1 :public ISprint0Task3
+
+{
+	virtual int SummV3(int a, int b, int c) override
+	{
+		return a + b + c;
+	};
+};
