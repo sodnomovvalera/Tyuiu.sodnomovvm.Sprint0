f42d3e325af9dd30cb18bc79867faba607220cad
tyuiu.sodnomovvm@outlook.com <deek.ren.gg@gmail.com>
2023-09-12 20:17:10 +0500

    Tusk4

---
+++ Tyuiu.sodnomovvm.Sprint0.Task4V0.Lib/Tyuiu.sodnomovvm.Sprint0.Task4V0.Lib.cpp
@@ -0,0 +1,11 @@
+﻿// Tyuiu.sodnomovvm.Sprint0.Task4V0.Lib.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "../../Tyuiu.Cours.cpp/Tyuiu.Cours.cpp.cpp"
+
+// TODO: Это пример библиотечной функции.
+void fnTyuiusodnomovvmSprint0Task4V0Lib()
+{
+}
