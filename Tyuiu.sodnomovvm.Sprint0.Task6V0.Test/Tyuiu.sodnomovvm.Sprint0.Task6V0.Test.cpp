4fcb773835a76354994a87395c77cba087771739
tyuiu.sodnomovvm@outlook.com <deek.ren.gg@gmail.com>
2023-09-13 22:33:50 +0500

    TAsk6

---
+++ Tyuiu.sodnomovvm.Sprint0.Task6V0.Test/Tyuiu.sodnomovvm.Sprint0.Task6V0.Test.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tyuiu.sodnomovvm.Sprint0.Task6V0.Lib/Tyuiu.sodnomovvm.Sprint0.Task6V0.Lib.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TyuiusodnomovvmSprint0Task6V0Test
+{
+	TEST_CLASS(TyuiusodnomovvmSprint0Task6V0Test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
