037d2cef9dfdfb4ad1f261e34e01318bce6e7ba2
tyuiu.sodnomovvm@outlook.com <deek.ren.gg@gmail.com>
2023-09-14 17:56:02 +0500

    TAsk7

---
+++ Tyuiu.sodnomovvm.Sprint0.TAsk7.V0.Lib/Tyuiu.sodnomovvm.Sprint0.TAsk7.V0.Lib.cpp
@@ -0,0 +1,11 @@
+﻿// Tyuiu.sodnomovvm.Sprint0.TAsk7.V0.Lib.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "../../Tyuiu.Cours.cpp/Tyuiu.Cours.cpp.cpp"
+
+// TODO: Это пример библиотечной функции.
+void fnTyuiusodnomovvmSprint0TAsk7V0Lib()
+{
+}
---
+++ Tyuiu.sodnomovvm.Sprint0.TAsk7.V0.Test/Tyuiu.sodnomovvm.Sprint0.TAsk7.V0.Test.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tyuiu.sodnomovvm.Sprint0.TAsk7.V0.Lib/Tyuiu.sodnomovvm.Sprint0.TAsk7.V0.Lib.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TyuiusodnomovvmSprint0TAsk7V0Test
+{
+	TEST_CLASS(TyuiusodnomovvmSprint0TAsk7V0Test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
