f3b25cbb2d1b3de99eb8eb2099724dc27a5a3ef1
tyuiu.sodnomovvm@outlook.com <deek.ren.gg@gmail.com>
2023-09-21 12:59:50 +0500

    Контрольная 0 Вариант 2 задача 2

---
+++ Tyuy.sodnomovvm.Control0.V2.Zadacha2.Lib/Tyuy.sodnomovvm.Control0.V2.Zadacha2.Lib.cpp
@@ -0,0 +1,11 @@
+﻿// Tyuy.sodnomovvm.Control0.V2.Zadacha2.Lib.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "../../Tyuiu.CoursControl_0/Tyuiu.CoursControl_0.cpp"
+
+// TODO: Это пример библиотечной функции.
+void fnTyuysodnomovvmControl0V2Zadacha2Lib()
+{
+}
---
+++ Tyuy.sodnomovvm.Control0.V2.Zadacha2.Test/Tyuy.sodnomovvm.Control0.V2.Zadacha2.Test.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tyuy.sodnomovvm.Control0.V2.Zadacha2.Lib/Tyuy.sodnomovvm.Control0.V2.Zadacha2.Lib.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TyuysodnomovvmControl0V2Zadacha2Test
+{
+	TEST_CLASS(TyuysodnomovvmControl0V2Zadacha2Test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
