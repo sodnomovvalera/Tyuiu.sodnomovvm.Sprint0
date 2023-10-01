c36e6e10c7136aa7b629e972b8e7674c3d2fdae7
tyuiu.sodnomovvm@outlook.com <deek.ren.gg@gmail.com>
2023-10-01 22:38:24 +0500

    Task5

---
+++ Tyuiu.SodnomovVM.Sprint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SodnomovVM.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Tyuiu.SodnomovVM.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2f88ed23-f67c-40a7-bbca-4be832297d09")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
