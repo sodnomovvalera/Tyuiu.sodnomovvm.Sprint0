c06ede7a4ca87149c6a3d72087d455aeec7ff5e2
tyuiu.sodnomovvm@outlook.com <deek.ren.gg@gmail.com>
2023-10-01 23:16:37 +0500

    Task6

---
+++ Tyuiu.SodnomovVM.Sprint0.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SodnomovVM.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Tyuiu.SodnomovVM.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("86936075-80be-401a-85fa-c99a33e4e982")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.SodnomovVM.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.SodnomovVM.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.SodnomovVM.Sprint0.Task6.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numArray = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(numArray));
+            Console.WriteLine("Разность элементов массива = " + DataService.SubtractionArray(numArray));
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(numArray));
+            Console.ReadKey();
+        }
+    }
+}
